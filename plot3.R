2ff91a55dc33d3ce51b0f82d1242ed10011e810d
Mohamed5167 <Mohamed5167@gmail.com>
2022-08-13 18:43:59 +0400

    Add plot3.R

---
+++ plot3.R
@@ -0,0 +1,19 @@
+plot3 <- function(data_file) {
+    data_electric <- read.table(data_file, header = TRUE, sep = ";",
+                                na.strings = "?")
+    data_electric$Date <- as.Date(data_electric$Date, "%d/%m/%Y")
+    new_data <- data_electric[data_electric$Date == ymd("2007-02-01")
+                              | data_electric$Date == ymd("2007-02-02"), ]
+    
+    df_date_time <- as.POSIXct(paste(new_data$Date, new_data$Time))
+    
+    
+    png(filename = "plot3.png", width = 480, height = 480)
+    plot(df_date_time, new_data$Sub_metering_1, type = "l", xlab = "",
+         ylab = "Energy sub metering")
+    lines(df_date_time, new_data$Sub_metering_2, col = "red")
+    lines(df_date_time, new_data$Sub_metering_3, col = "blue")
+    legend("topright", legend = tail(colnames(new_data), n = 3), lty = 1,
+           col = c("black", "red", "blue"))
+    dev.off()
+}
